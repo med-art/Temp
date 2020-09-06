5ed7591d674195c1cb484698437564a9b1ebefe2
windowtrap <38334931+windowtrap@users.noreply.github.com>
2020-09-06 12:01:05 +1200

    Update test.js

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ function setup() {
 
   canvas.addEventListener('touchmove', moved);
   canvas.addEventListener('mousemove', moved);
-  canvas.addEventListener('touchstart', moved);
-  canvas.addEventListener('mousedown', moved);
-  canvas.addEventListener('touchend', moved);
-  canvas.addEventListener('touchleave', moved);
-  canvas.addEventListener('mouseup', moved);
+  // canvas.addEventListener('touchstart', moved);
+  // canvas.addEventListener('mousedown', moved);
+  // canvas.addEventListener('touchend', moved);
+  // canvas.addEventListener('touchleave', moved);
+  // canvas.addEventListener('mouseup', moved);
 
   strokeWeight(10);
   stroke(250);
